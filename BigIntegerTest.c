89244c07133bee213d774f4ac65959aa77f20a27
Jay Montoya <jayjoplin9898@.com>
2019-11-05 15:11:39 -0800

    fx

--- BigIntegerTest.c
+++ BigIntegerTest.c
@@ -17,18 +17,15 @@ int main(int argc, char* argv[]) {
   // create a big integer list
   BigInteger A;
 
-  char myString1[] = "-000010209390";
-  char myString2[] = "+00001020939000";
-  char myString3[] = "-010209390";
-  char myString4[] = "-4324879";
-  char myString5[] = "93280";
-  char myString6[] = "11";
-  char myString7[] = "0";
-  char myString8[] = "+22";
+
   char myString9[] = "-0";
-  char myString10[] = "-11";
 
   A = stringToBigInteger(myString9);
+
+  free(myString9);
+  myString9 = NULL;
+
+
 
   // free memory associated with the big integer
   freeBigInteger(&A);
