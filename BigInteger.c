cd4aac3db14acdbda041a94697348984ad57c327
Jay Montoya <jayjoplin9898@.com>
2019-11-10 19:24:32 -0800

    fx

--- BigInteger.c
+++ BigInteger.c
@@ -861,7 +861,7 @@ void normalize(BigInteger N) {
       // if the index is out of bounds.. determine by how much
       if (get(NList) < 0) {
         // determine how many borrows were needed..
-        carry = (get(NList) / BASE); // will product a negative carry in this case
+        carry = (get(NList) / BASE) + 1; // will product a negative carry in this case
         set(NList, get(NList) + (BASE * carry));
       } else if (get(NList) >= BASE) {
         printf("\ncarry applicable!\n");
