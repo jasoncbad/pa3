44212d7f4d3ae69165a893b10baca68d2f2a959b
Jay Montoya <jayjoplin9898@.com>
2019-11-10 16:27:51 -0800

    fx

--- BigInteger.c
+++ BigInteger.c
@@ -777,7 +777,7 @@ BigInteger diff(BigInteger A, BigInteger B) {
      B = Temp;
      S->sign = -1;
    } else if (comparison > 0) {
-     if (sign(B) == -1) {
+     if ((sign(B) == -1 && sign(A) == 1) || (sign(B) == -1 && sign(A) == -1)) {
        printf("\nB is negative and less than S..");
        free(S);
        negate(B);
@@ -785,7 +785,7 @@ BigInteger diff(BigInteger A, BigInteger B) {
        S->sign = 1;
        negate(B);
        return S;
-     }
+     } 
    } else {
       S->sign = 1;
    }
