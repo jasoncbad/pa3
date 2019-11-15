7ff9212295241da1da21e01363fe3d07865b7619
Jay Montoya <jayjoplin9898@.com>
2019-11-14 19:06:27 -0800

    fx

--- List.c
+++ List.c
@@ -178,14 +178,14 @@ void append(List L, long data) {
   }
 
 
-/*
+
   if (length(L) == 0) {
     // code for inserting a one and only node
     prepend(L, data); // recycle code!
-    (L->length)--;
+    //(L->length)--;
     return;
   }
-*/
+
 
   // otherwise length is > 0
   // code for inserting a node AFTER the last node
